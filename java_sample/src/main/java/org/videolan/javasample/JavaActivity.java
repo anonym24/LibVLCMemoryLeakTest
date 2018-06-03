6195db5da18ccb9aef43f1e3a17fe5ddfbe17930
Anonym <willalwaysdie@gmail.com>
2018-06-03 13:17:25 +0300

    First commit

--- java_sample/src/main/java/org/videolan/javasample/JavaActivity.java
+++ java_sample/src/main/java/org/videolan/javasample/JavaActivity.java
@@ -2,7 +2,6 @@ package org.videolan.javasample;
 
 import android.annotation.TargetApi;
 import android.content.res.Configuration;
-import android.graphics.PixelFormat;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Handler;
@@ -10,7 +9,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.SurfaceView;
-import android.view.TextureView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewStub;
@@ -24,22 +22,12 @@ import org.videolan.libvlc.MediaPlayer;
 import java.util.ArrayList;
 
 public class JavaActivity extends AppCompatActivity implements IVLCVout.OnNewVideoLayoutListener {
-    private static final boolean USE_SURFACE_VIEW = true;
-    private static final boolean ENABLE_SUBTITLES = true;
     private static final String TAG = "JavaActivity";
     private static final String SAMPLE_URL = "http://download.blender.org/peach/bigbuckbunny_movies/BigBuckBunny_640x360.m4v";
-    private static final int SURFACE_BEST_FIT = 0;
-    private static final int SURFACE_FIT_SCREEN = 1;
-    private static final int SURFACE_FILL = 2;
-    private static final int SURFACE_16_9 = 3;
-    private static final int SURFACE_4_3 = 4;
-    private static final int SURFACE_ORIGINAL = 5;
-    private static int CURRENT_SIZE = SURFACE_BEST_FIT;
 
     private FrameLayout mVideoSurfaceFrame = null;
     private SurfaceView mVideoSurface = null;
     private SurfaceView mSubtitlesSurface = null;
-    private TextureView mVideoTexture = null;
     private View mVideoView = null;
 
     private Handler mHandler;
@@ -54,33 +42,15 @@ public class JavaActivity extends AppCompatActivity implements IVLCVout.OnNewVid
     private int mVideoSarNum = 0;
     private int mVideoSarDen = 0;
 
-    //private File file;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         mHandler = new Handler();
         setContentView(R.layout.activity_main);
-
-        //requestPermissions(new String[] { WRITE_EXTERNAL_STORAGE }, 1);
-        //file = new File(Environment.getExternalStorageDirectory() + File.separator + "test5.mkv");
-
         mVideoSurfaceFrame = findViewById(R.id.video_surface_frame);
-        if (USE_SURFACE_VIEW) {
-            ViewStub stub = findViewById(R.id.surface_stub);
-            mVideoSurface = (SurfaceView) stub.inflate();
-            if (ENABLE_SUBTITLES) {
-                stub = findViewById(R.id.subtitles_surface_stub);
-                mSubtitlesSurface = (SurfaceView) stub.inflate();
-                mSubtitlesSurface.setZOrderMediaOverlay(true);
-                mSubtitlesSurface.getHolder().setFormat(PixelFormat.TRANSLUCENT);
-            }
-            mVideoView = mVideoSurface;
-        } else {
-            ViewStub stub = findViewById(R.id.texture_stub);
-            mVideoTexture = (TextureView) stub.inflate();
-            mVideoView = mVideoTexture;
-        }
+        ViewStub stub = findViewById(R.id.surface_stub);
+        mVideoSurface = (SurfaceView) stub.inflate();
+        mVideoView = mVideoSurface;
     }
 
     @Override
@@ -115,8 +85,6 @@ public class JavaActivity extends AppCompatActivity implements IVLCVout.OnNewVid
             if (mSubtitlesSurface != null)
                 vlcVout.setSubtitlesView(mSubtitlesSurface);
         }
-        else
-            vlcVout.setVideoView(mVideoTexture);
         vlcVout.attachViews(this);
 
         //Media media = new Media(mLibVLC, SAMPLE_URL);
@@ -163,79 +131,15 @@ public class JavaActivity extends AppCompatActivity implements IVLCVout.OnNewVid
         }
     }
 
-    private void changeMediaPlayerLayout(int displayW, int displayH) {
-        /* Change the video placement using the MediaPlayer API */
-        switch (CURRENT_SIZE) {
-            case SURFACE_BEST_FIT:
-                mMediaPlayer.setAspectRatio(null);
-                mMediaPlayer.setScale(0);
-                break;
-            case SURFACE_FIT_SCREEN:
-            case SURFACE_FILL: {
-                Media.VideoTrack vtrack = mMediaPlayer.getCurrentVideoTrack();
-                if (vtrack == null)
-                    return;
-                final boolean videoSwapped = vtrack.orientation == Media.VideoTrack.Orientation.LeftBottom
-                        || vtrack.orientation == Media.VideoTrack.Orientation.RightTop;
-                if (CURRENT_SIZE == SURFACE_FIT_SCREEN) {
-                    int videoW = vtrack.width;
-                    int videoH = vtrack.height;
-
-                    if (videoSwapped) {
-                        int swap = videoW;
-                        videoW = videoH;
-                        videoH = swap;
-                    }
-                    if (vtrack.sarNum != vtrack.sarDen)
-                        videoW = videoW * vtrack.sarNum / vtrack.sarDen;
-
-                    float ar = videoW / (float) videoH;
-                    float dar = displayW / (float) displayH;
-
-                    float scale;
-                    if (dar >= ar)
-                        scale = displayW / (float) videoW; /* horizontal */
-                    else
-                        scale = displayH / (float) videoH; /* vertical */
-                    mMediaPlayer.setScale(scale);
-                    mMediaPlayer.setAspectRatio(null);
-                } else {
-                    mMediaPlayer.setScale(0);
-                    mMediaPlayer.setAspectRatio(!videoSwapped ? ""+displayW+":"+displayH
-                            : ""+displayH+":"+displayW);
-                }
-                break;
-            }
-            case SURFACE_16_9:
-                mMediaPlayer.setAspectRatio("16:9");
-                mMediaPlayer.setScale(0);
-                break;
-            case SURFACE_4_3:
-                mMediaPlayer.setAspectRatio("4:3");
-                mMediaPlayer.setScale(0);
-                break;
-            case SURFACE_ORIGINAL:
-                mMediaPlayer.setAspectRatio(null);
-                mMediaPlayer.setScale(1);
-                break;
-        }
-    }
-
     private void updateVideoSurfaces() {
         int sw = getWindow().getDecorView().getWidth();
         int sh = getWindow().getDecorView().getHeight();
-
-        // sanity check
         if (sw * sh == 0) {
-            Log.e(TAG, "Invalid surface size");
             return;
         }
-
         mMediaPlayer.getVLCVout().setWindowSize(sw, sh);
-
         ViewGroup.LayoutParams lp = mVideoView.getLayoutParams();
         if (mVideoWidth * mVideoHeight == 0) {
-            /* Case of OpenGL vouts: handles the placement of the video using MediaPlayer API */
             lp.width  = ViewGroup.LayoutParams.MATCH_PARENT;
             lp.height = ViewGroup.LayoutParams.MATCH_PARENT;
             mVideoView.setLayoutParams(lp);
@@ -243,87 +147,41 @@ public class JavaActivity extends AppCompatActivity implements IVLCVout.OnNewVid
             lp.width  = ViewGroup.LayoutParams.MATCH_PARENT;
             lp.height = ViewGroup.LayoutParams.MATCH_PARENT;
             mVideoSurfaceFrame.setLayoutParams(lp);
-            changeMediaPlayerLayout(sw, sh);
+            mMediaPlayer.setAspectRatio(null);
+            mMediaPlayer.setScale(0);
             return;
         }
-
         if (lp.width == lp.height && lp.width == ViewGroup.LayoutParams.MATCH_PARENT) {
-            /* We handle the placement of the video using Android View LayoutParams */
             mMediaPlayer.setAspectRatio(null);
             mMediaPlayer.setScale(0);
         }
-
         double dw = sw, dh = sh;
         final boolean isPortrait = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
-
         if (sw > sh && isPortrait || sw < sh && !isPortrait) {
             dw = sh;
             dh = sw;
         }
-
-        // compute the aspect ratio
         double ar, vw;
         if (mVideoSarDen == mVideoSarNum) {
-            /* No indication about the density, assuming 1:1 */
-            vw = mVideoVisibleWidth;
             ar = (double)mVideoVisibleWidth / (double)mVideoVisibleHeight;
         } else {
-            /* Use the specified aspect ratio */
             vw = mVideoVisibleWidth * (double)mVideoSarNum / mVideoSarDen;
             ar = vw / mVideoVisibleHeight;
         }
-
-        // compute the display aspect ratio
         double dar = dw / dh;
-
-        switch (CURRENT_SIZE) {
-            case SURFACE_BEST_FIT:
-                if (dar < ar)
-                    dh = dw / ar;
-                else
-                    dw = dh * ar;
-                break;
-            case SURFACE_FIT_SCREEN:
-                if (dar >= ar)
-                    dh = dw / ar; /* horizontal */
-                else
-                    dw = dh * ar; /* vertical */
-                break;
-            case SURFACE_FILL:
-                break;
-            case SURFACE_16_9:
-                ar = 16.0 / 9.0;
-                if (dar < ar)
-                    dh = dw / ar;
-                else
-                    dw = dh * ar;
-                break;
-            case SURFACE_4_3:
-                ar = 4.0 / 3.0;
-                if (dar < ar)
-                    dh = dw / ar;
-                else
-                    dw = dh * ar;
-                break;
-            case SURFACE_ORIGINAL:
-                dh = mVideoVisibleHeight;
-                dw = vw;
-                break;
-        }
-
-        // set display size
+        if (dar < ar)
+            dh = dw / ar;
+        else
+            dw = dh * ar;
         lp.width  = (int) Math.ceil(dw * mVideoWidth / mVideoVisibleWidth);
         lp.height = (int) Math.ceil(dh * mVideoHeight / mVideoVisibleHeight);
         mVideoView.setLayoutParams(lp);
         if (mSubtitlesSurface != null)
             mSubtitlesSurface.setLayoutParams(lp);
-
-        // set frame size (crop if necessary)
         lp = mVideoSurfaceFrame.getLayoutParams();
         lp.width = (int) Math.floor(dw);
         lp.height = (int) Math.floor(dh);
         mVideoSurfaceFrame.setLayoutParams(lp);
-
         mVideoView.invalidate();
         if (mSubtitlesSurface != null)
             mSubtitlesSurface.invalidate();
